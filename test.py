1d81bfa9df0c2122d4d8f2a92900d40068819138
ElinorG11 <48283282+ElinorG11@users.noreply.github.com>
2021-11-18 15:26:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from torch import Tensor
 from aihwkit.nn import AnalogLinear
 
+# Test example to make sure I can activate the kit
+
 model = AnalogLinear(2, 2)
 model(Tensor([[0.1, 0.2], [0.3, 0.4]]))
